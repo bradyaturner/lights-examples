79fdc670046eb47e3cc8f8fc87c2e8e5b23d6fe3
Brady Turner <bturner@nuvotechnologies.com>
2015-01-01 14:41:49 -0500

    use hue constants from lights gem

--- animations.rb
+++ animations.rb
@@ -3,34 +3,27 @@
 require 'lights'
 
 LIGHTS_CONFIG_PATH = "#{ENV["HOME"]}/.lightsconfig"
-
-YELLOW = 12750
-LGREEN = 22500
-GREEN = 25500
-BLUE = 46920
-PURPLE = 56100
-RED = 65535
-
 DEFAULT_DELAY = 5
 
 @config = {}
 if File.exists? LIGHTS_CONFIG_PATH
   @config = JSON.parse( IO.read( LIGHTS_CONFIG_PATH ) )
 end
+
 hue = Lights.new @config["bridge_ip"], @config["username"]
 
 delay = ARGV[0] ? ARGV[0].to_i : DEFAULT_DELAY
 
 b = BulbState.new
-b.transition_time = delay*10
 b.sat = BulbState::MAX_SAT
+b.bri = BulbState::MAX_BRI
+b.transition_time = delay*10
 
-colors = [ YELLOW, LGREEN, GREEN,
-            BLUE, PURPLE, RED ]
+puts "Press ctrl+c to stop."
 
 while TRUE
-  colors.each do |c|
-    b.hue = c
+  BulbState::Hue.constants.each do |c|
+    b.hue = BulbState::Hue.const_get c
     hue.set_group_state(0,b)
     sleep delay
   end
