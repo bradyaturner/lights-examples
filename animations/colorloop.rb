c1ddad69bd8637a74e87376a40c4522ce61f26db
Brady Turner <bturner@nuvotechnologies.com>
2015-01-01 16:43:25 -0500

    turn on for colorloop

--- animations/colorloop.rb
+++ animations/colorloop.rb
@@ -15,6 +15,7 @@ hue = Lights.new @config["bridge_ip"], @config["username"]
 delay = ARGV[0] ? ARGV[0].to_i : DEFAULT_DELAY
 
 b = BulbState.new
+b.on = true
 b.sat = BulbState::MAX_SAT
 b.bri = BulbState::MAX_BRI
 b.transition_time = delay*10
